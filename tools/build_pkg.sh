6af128fb058dc2610544359dc16fdd070ff5f9ff
Azalea Gui <22280294+hykilpikonna@users.noreply.github.com>
2024-10-23 15:12:44 -0400

    [U] Update build script

--- tools/build_pkg.sh
+++ tools/build_pkg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FASTFETCH_VERSION="2.12.0"
+FASTFETCH_VERSION="2.28.0"
 FASTFETCH_DL="https://github.com/fastfetch-cli/fastfetch/releases/download/$FASTFETCH_VERSION/"
 
 # Get script directory
@@ -109,11 +109,13 @@ function build_for_platform() {
 # The official fastfetch build uses Ubuntu 20.04 with glibc 2.31
 build_for_platform "linux-amd64" "manylinux_2_31_x86_64"
 build_for_platform "linux-aarch64" "manylinux_2_31_aarch64"
+build_for_platform "linux-armv7l" "manylinux_2_31_armv7l"
+# build_for_platform "linux-riscv64" "manylinux_2_31_riscv64"
 # There doesn't seem to be tags for freebsd?
 # build_for_platform "freebsd-amd64" "freebsd_x86_64"
 # build_for_platform "freebsd-aarch64" "freebsd_aarch64"
 build_for_platform "musl-amd64" "musllinux_1_1_x86_64"
-build_for_platform "musl-aarch64" "musllinux_1_1_aarch64"
+# build_for_platform "musl-aarch64" "musllinux_1_1_aarch64"
 # The official fastfetch build uses macOS 12.0
 build_for_platform "macos-universal" "macosx_11_0_x86_64"
 build_for_platform "macos-universal" "macosx_11_0_arm64"
