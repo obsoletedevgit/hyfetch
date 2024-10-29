aa065fe910290e0fcc426d4a55cfc73bad0b2b6d
Lubos Kocman <Lubos.Kocman@suse.com>
2024-10-29 14:28:26 +0100

    openSUSE distro refresh
    * Add openSUSE Slowroll
    * Updated logos for openSUSE Leap and Tumbleweed
      gh#openSUSE/branding#154 gh#openSUSE/branding#160
    * Update neofetch accordingly to gh#openSUSE/neofetch fork

--- hyfetch/distros/opensuse_tumbleweed.py
+++ hyfetch/distros/opensuse_tumbleweed.py
@@ -3,18 +3,27 @@
 from . import AsciiArt
 
 opensuse_tumbleweed = AsciiArt(match=r'''"openSUSE Tumbleweed"* | "openSUSE_Tumbleweed"*''', color='2 7', ascii=r"""
-${c2}                                     ......
-     .,cdxxxoc,.               .:kKMMMNWMMMNk:.
-    cKMMN0OOOKWMMXo. A        ;0MWk:'      ':OMMk.
-  ;WMK;'       'lKMMNM,     :NMK'             'OMW;
- cMW;             WMMMN   ,XMK'                 oMM.
-.MMc             ''^*~l. xMN:                    KM0
-'MM.                   .NMO                      oMM
-.MM,                 .kMMl                       xMN
- KM0               .kMM0' .dl>~,.               .WMd
- 'XM0.           ,OMMK'    OMMM7'              .XMK
-   *WMO:.    .;xNMMk'       NNNMKl.          .xWMx
-     ^ONMMNXMMMKx;          V  'xNMWKkxllox0NMWk'
-         '''''                    ':dOOXXKOxl'
+${c2}         JJJJJJJJ
+      JJJJJJJJJJJJJJ
+    JJJJJJ   =JJJJJJJ
+   JJJJ      =JJJ JJJJ
+   JJJ       =JJJ   JJJ
+  JJJJ       =JJJ   JJJ
+  JJJJJJJJJJJJJJJ   JJJJ
+   JJJJJJJJJJJJJJ   JJJJ
+   JJJJ             JJJJ
+    JJJJJ=          JJJJ
+      JJJJJJJJJJJJJJJJJJJJJJJJJJJJJ=
+        =JJJJJJJJJJJJJJJJJJJJJJJJJJJJJ
+                    JJJJ         =JJJJJJ
+                    JJJJ            =JJJJ
+                    JJJJ   JJJJJJJJJJJJJJ
+                    JJJJ   JJJJJJJJJJJJJJJ
+                    JJJJ   JJJJ       JJJJ
+                     JJJ   JJJJ       JJJ
+                     JJJJJ JJJJ      JJJJ
+                      =JJJJJJJJ   JJJJJJ
+                        JJJJJJJJJJJJJJ
+                           JJJJJJJ=
 """)
     
