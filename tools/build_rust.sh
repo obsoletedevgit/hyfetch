afd9ad33d49744cea35441534fdd1fdc47ad00bc
Azalea Gui <22280294+hykilpikonna@users.noreply.github.com>
2024-12-22 01:39:13 -0500

    [F] Fix rust builder

--- tools/build_rust.sh
+++ tools/build_rust.sh
@@ -8,4 +8,4 @@ docker build -t rust-builder-general tools/rust-builder/general
 
 # Step 2: Run the Docker container and execute the builder script
 echo "Running the Docker container..."
-docker run --rm -v "$(pwd)":/app rust-builder bash -c "cd /app && ./tools/rust-builder/general/builder.sh"
+docker run --rm -v "$(pwd)":/app rust-builder-general bash -c "cd /app && ./tools/rust-builder/general/build.sh"
--- tools/rust-builder/general/build.sh
+++ tools/rust-builder/general/build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Set linkers for cross-compilation
+export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=/usr/bin/arm-linux-gnueabihf-gcc
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=/usr/bin/aarch64-linux-gnu-gcc
+
 # Build for platforms
 cargo build --release --target x86_64-unknown-linux-gnu
 cargo build --release --target aarch64-unknown-linux-gnu
