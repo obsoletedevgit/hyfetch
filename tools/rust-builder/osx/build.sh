b009342d78590eec28f291425e4cab9b7be42773
Azalea Gui <22280294+hykilpikonna@users.noreply.github.com>
2024-12-22 05:53:46 -0500

    [F] Fix osx build strip issue

--- tools/rust-builder/osx/build.sh
+++ tools/rust-builder/osx/build.sh
@@ -13,9 +13,7 @@ export CARGO_TARGET_X86_64_APPLE_DARWIN_AR=/osxcross/bin/x86_64-apple-darwin$OSX
 export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=/osxcross/bin/aarch64-apple-darwin$OSX_VER-clang
 export CARGO_TARGET_AARCH64_APPLE_DARWIN_AR=/osxcross/bin/aarch64-apple-darwin$OSX_VER-ar
 
-
 # Build for platforms
-#cp /osxcross/bin/x86_64-apple-darwin$OSX_VER-strip /usr/bin/strip
-cargo build --release --target x86_64-apple-darwin
-#cp /osxcross/bin/aarch64-apple-darwin$OSX_VER-strip /usr/bin/strip
-cargo build --release --target aarch64-apple-darwin
+# +beta because rust doesn't know which strip to use until version 1.84
+cargo +beta build --release --target x86_64-apple-darwin
+cargo +beta build --release --target aarch64-apple-darwin
