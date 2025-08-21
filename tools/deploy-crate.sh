3c7c449da48d3831b12d1d7b76406b748ce4016a
Azalea Gui <22280294+hykilpikonna@users.noreply.github.com>
2025-08-21 09:28:44 -0400

    [F] Maybe this can work

---
+++ tools/deploy-crate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# This script does some file path magic to make cargo publish work without having
+# to have neofetch & font logos in the cargo root.
+# ...okay basically it copies these files over before publishing.
+set -euo pipefail
+
+# Get the directory of SRC root which is ../ from this script
+SRC_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+CARGO_ROOT="$SRC_ROOT/crates/hyfetch"
+
+# Copy neofetch from here to cargo root
+cp "$SRC_ROOT/neofetch" "$CARGO_ROOT/"
+mkdir -p "$CARGO_ROOT/hyfetch"
+cp -r "$SRC_ROOT/hyfetch/data" "$CARGO_ROOT/hyfetch/"
+
+# Build the crate
+cargo publish -p hyfetch --dry-run --allow-dirty
+
+# Delete the copied files
+rm "$CARGO_ROOT/neofetch"
+rm -rf "$CARGO_ROOT/hyfetch"
+
+echo "Done!"
