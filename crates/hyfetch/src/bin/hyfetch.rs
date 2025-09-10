ef1407d00e9002eb21078ea6babab936b9f39e8c
ObsoleteDev <91698052+obsoletedevgit@users.noreply.github.com>
2025-09-10 15:49:32 +0100

    [F] Only mark pride month easter egg as displayed when its june (rust) (#430)
    fix(rust): Only mark pride month easter egg as displayed when its june

--- crates/hyfetch/src/bin/hyfetch.rs
+++ crates/hyfetch/src/bin/hyfetch.rs
@@ -117,7 +117,7 @@ fn main() -> Result<()> {
         )
         .context("failed to write message to stdout")?;
 
-        if !june_path.is_file() {
+        if !june_path.is_file() && !options.june {
             File::create(&june_path)
                 .with_context(|| format!("failed to create file {june_path:?}"))?;
         }
